3fcfbecc58013ec8803e656d77bf6b1ecce5466a
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2025-02-12 17:07:58 +0100

    Ajout des fichiers nécessaires

--- converter.py
+++ converter.py
@@ -91,7 +91,10 @@ def download_pdf(url):
 
     filename = os.path.join(DOWNLOAD_FOLDER, clean_filename)
 
-    response = requests.get(url, stream=True)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers, stream=True)
     if response.status_code == 200:
         with open(filename, "wb") as f:
             for chunk in response.iter_content(chunk_size=1024):
